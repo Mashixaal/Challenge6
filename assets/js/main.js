fb483262738a9d75ee8c57ebb89142655de7f890
mashixaal <safwan7342@gmail.com>
2022-08-01 16:54:32 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+let button = document.getElementById("liveAlertBtn");
+
+const body = document.querySelector("body");
+
+console.log(body);
+
+const text = () => {
+  let p = document.createElement("p");
+  p.innerHTML = "lorem";
+  body.append(p);
+};
+
+button.addEventListener("click", text);
+
+console.log(button);
